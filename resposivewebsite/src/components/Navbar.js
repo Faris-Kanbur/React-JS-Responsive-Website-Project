5d098c73c01eee47c1069023c1a6b1f6b41f7871
faris <faris38kanbur@gmail.com>
2021-01-03 13:52:36 +0100

    add Hero components

--- resposivewebsite/src/components/Navbar.js
+++ resposivewebsite/src/components/Navbar.js
@@ -29,8 +29,9 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
+            One
             <i class='fas fa-book-reader' />
+            Page
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
