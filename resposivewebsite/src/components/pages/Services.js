7f49f0e656605d1cb08a98561795cb264541c3a5
faris <faris38kanbur@gmail.com>
2021-05-05 23:41:19 +0200

    add setvice

--- resposivewebsite/src/components/pages/Services.js
+++ resposivewebsite/src/components/pages/Services.js
@@ -1,125 +1,102 @@
 import React from 'react';
-import '../../App.css';
-import '../Service.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, AppBar,Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container  } from '@material-ui/core';
-import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: 'auto',
+    marginTop: '25%'
   },
-  icon: {
-      marginRight: theme.spacing(2),  // '20px'
+  media: {
+    height: 140,
   },
-  buttons: {
-      marginTop: '40px',
-  },
-  cardGrid: {
-      marginTop: '20px 0'
-  },
-  card: {
-      height: '75%',
-      display: 'flex',
-      flexDirection: 'column',
-      marginTop: '65%',
-      
-  },
-  cardMedia :{
-      paddingTop: '99.25%',
-    
-  },
-  cardContent:{
-      flexGrow: 1,
-  },
-
-}));
+});
 
 export default function MediaCard() {
   const classes = useStyles();
 
   return (
-    <div className="services">
-                  
-              <Container className={classes.cardGrid} maxWidth="md">
-                <Grid container spacing={4}>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Card className={classes.card} >
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image="../images/emre.jpg"
-                                title="Image title"
-                            />
-                            <CardContent className={classes.CardContent}>
-                                <Typography gutterBottom variant="h5" color="primary">
-                                    Emre Kurt
-                                </Typography>
-                                <Typography gutterBottom variant="h6" color='textSecondary'>
-                                  Full Stack Developer
-                                </Typography>
-                                <Typography>
-                                    This is media card. You can use this section to describe the content.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">View</Button>
-                                <Button size="small" color="primary">Edit</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="../images/faris.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
 
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Card className={classes.card} >
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image="../images/faris.jpg"
-                                title="Image title"
-                            />
-                            <CardContent className={classes.CardContent}>
-                                <Typography gutterBottom variant="h5" color="primary">
-                                    Faris Kanbur
-                                </Typography>
-                                <Typography gutterBottom variant="h6" color='textSecondary'>
-                                  Full Stack Developer
-                                </Typography>
-                                <Typography>
-                                    This is media card. You can use this section to describe the content.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">View</Button>
-                                <Button size="small" color="primary">Edit</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Card className={classes.card} >
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image="../images/hüso.jpeg"
-                                title="Image title"
-                            />
-                            <CardContent className={classes.CardContent}>
-                                <Typography gutterBottom variant="h5" color="primary">
-                                    Hüseyin Gülçiçek
-                                </Typography>
-                                <Typography gutterBottom variant="h6" color='textSecondary'>
-                                  Data Analyst
-                                </Typography>
-                                <Typography>
-                                    This is media card. You can use this section to describe the content.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">View</Button>
-                                <Button size="small" color="primary">Edit</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                
-                </Grid>
-              </Container>
-    </div>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="../images/faris.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="../images/faris.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
